fad76a6a8fdb15ea34697c02085c63888584897c
Christopher Higgins <chiggi24@uwo.ca>
2023-10-14 14:33:04 -0400

    Added match functionality for superhero info and added get requests for superhero powers
    Added match function to get a specified field, match and number of results entered in the url string.
    
    Added functionality for get requests on both all the superhero_powers data and data based on a superhero id.

--- server/server.js
+++ server/server.js
@@ -3,17 +3,32 @@ const app = express();
 const superhero_util = require('./script.js');
 const port = process.env.PORT || 3000;
 
-
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
-
 app.get("/api/superheros_info", (req, res) => {
     res.send(superhero_util.get_all_info());
 });
 
+app.get("/api/superheros_info/match", (req, res) => {
+    const field = req.query.field;
+    const match = req.query.match;
+    const n = req.query.n;
+    if (n !== undefined) {
+        res.send(superhero_util.match(field, match, n));
+    }
+    else {
+        res.send(superhero_util.match(field, match));
+    }
+});
+
 app.get("/api/superheros_info/:id", (req, res) => {
     res.send(superhero_util.get_info(req.params.id));
+});
+
+app.get("/api/superheros_powers", (req, res) => {
+    res.send(superhero_util.get_all_powers());
+});
+
+app.get("/api/superheros_powers/:id", (req, res) => {
+    res.send(superhero_util.get_powers(req.params.id));
 });
 
 
