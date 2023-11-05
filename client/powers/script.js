48373974ef2c5f40272cad4aff6c145f73d7c11c
Christopher Higgins <chiggi24@uwo.ca>
2023-11-05 17:22:10 -0500

    Overhauled powers
    Changed HTML to nicely organize content, and pass W3C tests.
    
    Adding comments to script file and CSS file.
    
    Made fetch method async.

--- client/powers/script.js
+++ client/powers/script.js
@@ -1,60 +1,60 @@
-const test = () => {
-    const body = document.getElementsByClassName("body")[0];
-    const list = document.getElementById("list");
-    const div = document.getElementById("test");
-    const header = document.getElementById("header");
-    const url = `${location.protocol}//${location.host}/api/superheros_powers`;
-    fetch(url)
-        .then(resp => {
-            if (!resp.ok) {
-                return resp.text();
+// Method to trigger onload that displays all superhero information
+const onload = async () => {
+    try {
+        // Initializing DOM elements
+        const body = document.getElementsByClassName("body")[0];
+        const list = document.getElementById("list");
+        // Creating URL and fetching 
+        const url = `${location.protocol}//${location.host}/api/superheros_powers`;
+        const resp = await fetch(url);
+        // Response is not ok (code 200)
+        if (!resp.ok) {
+            // Alert with response text
+            const errorText = await resp.text();
+            alert(errorText);
+            return;
+        }
+        // Get JSON data from response
+        const data = await resp.json();
+        // Add data to HTML
+        for (let i = 0; i < data.length; i++) {
+            // Initializing DOM elements
+            const list_element = document.createElement("li");
+            const text_content = document.createTextNode(`${data[i].hero_names}'s Superhero Powers:`);
+            const new_list = document.createElement("ul");
+            // Organizing content
+            list_element.appendChild(text_content);
+            list.appendChild(list_element);
+            list_element.appendChild(new_list);
+            // Getting true powers for data
+            let powers = get_true_properties(data[i]);
+            // Adding powers to HTML
+            for (let j = 0; j < powers.length; j++) {
+                let new_list_element = document.createElement("li");
+                let textNode = document.createTextNode(powers[j]);
+                new_list_element.appendChild(textNode);
+                new_list.appendChild(new_list_element);
             }
-            return resp.json();
-        }).then((data) => {
-            if (typeof data === "string") {
-                const p = document.createElement("p");
-                p.appendChild(document.createTextNode(data))
-                div.insertBefore(p, div.firstChild);
-                body.style.visibility = "visible";
-                return;
-            }
-            for (let i = 0; i < data.length; i++) {
-                let list_items = [];
-                const para = document.createElement("p").appendChild(document.createTextNode(`${data[i].hero_names}'s Superhero Powers:`));
-                const list = document.createElement("ul");
-                header.appendChild(para);
-                header.appendChild(list);
-                let powers = get_true_properties(data[i]);
-                body.style.visibility = "visible";
-                for (let j = 0; j < powers.length; j++) {
-                    let list_element = document.createElement("li");
-                    list_items.push(list_element);
-                    let textNode = document.createTextNode(powers[j]);
-                    list_items[j].appendChild(textNode);
-                    list.appendChild(list_items[j]);
-                }
-            }
-            body.style.visibility = "visible";
-        }).catch((data) => {
-            console.log("Failed to convert response to JSON/text!")
-        })
-        .catch((error) => {
-            console.log("Failed Fetch!")
-        });
-
+        }
+        // Unhide content
+        body.style.visibility = "visible";
+    } catch (error) {
+        alert("An error occurred: " + error);
+    }
 };
+
+// Method that gets true (string) properties for a given object
 const get_true_properties = obj => {
     let true_properties = [];
-
     for (const prop in obj) {
         if (obj.hasOwnProperty(prop) && obj[prop] === "True") {
             true_properties.push(prop);
         }
     }
-
     return true_properties;
 }
 
+// Method to redirect to homepage
 const home = () => {
     window.location.href = "/";
 };
