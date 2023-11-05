578186b5bfe8872ab29d37427e000880755ac848
Christopher Higgins <chiggi24@uwo.ca>
2023-11-05 18:24:27 -0500

    Overhauled task1
    Added CSS and adjust HTML to fix structuring and display details nicely.
    
    Made onload function async and adjusted try/catch to capture exceptional behaviour better.
    
    HTML passes W3C validator

--- client/task1/script.js
+++ client/task1/script.js
@@ -1,49 +1,42 @@
+// Method to trigger onload that displays superhero information for given id
 const onload = async () => {
+    // Initializing DOM elements
     const paragraph = document.getElementById("paragraph");
     const body = document.getElementsByClassName("body")[0];
-    const list = document.getElementById("list");
-    const div = document.getElementById("test");
     const id = window.location.href.split("id=")[1];
-    
+    let data;
+    // id is undefined, display error to screen
     if (id === "") {
-        const p = document.createElement("p");
-        p.appendChild(document.createTextNode(`"ID" is not allowed to be empty`));
-        div.insertBefore(p, div.firstChild);
-        body.style.visibility = "visible";
-        return;
+        paragraph.appendChild(document.createTextNode(`"ID" is not allowed to be empty`));
     }
-    
-    const url = `${location.protocol}//${location.host}/api/superheros_info/${id}`;
-    
-    try {
-        const resp = await fetch(url);
-        
-        if (!resp.ok) {
-            const errorText = await resp.text();
-            const p = document.createElement("p");
-            p.appendChild(document.createTextNode(errorText));
-            div.insertBefore(p, div.firstChild);
-            body.style.visibility = "visible";
-            return;
+    else {
+        // Creating URL
+        const url = `${location.protocol}//${location.host}/api/superheros_info/${id}`;
+        try {
+            // Fetching URL
+            const resp = await fetch(url);
+            // Response is not ok, display error message on screen
+            if (!resp.ok) {
+                const errorText = await resp.text();
+                paragraph.appendChild(document.createTextNode(errorText));
+                body.style.visibility = "visible";
+                return;
+            }
+            // Get JSON data from response
+            data = await resp.json();
+        } catch (error) {
+            console.log("An error occurred: " + error);
         }
-        
-        const data = await resp.json();
-        
-        if (typeof data === "string") {
-            const p = document.createElement("p");
-            p.appendChild(document.createTextNode(data));
-            div.insertBefore(p, div.firstChild);
-            body.style.visibility = "visible";
-            return;
-        }
-        
-        paragraph.appendChild(document.createTextNode(`${data.name}'s Superhero Information (ID: ${id}):`));
+        // Initializing DOM elements
+        const text_content = document.createTextNode(`${data.name}'s Superhero Information (ID: ${id}):`);
+        const new_list = document.createElement("ul");
+        paragraph.appendChild(text_content);
+        paragraph.appendChild(new_list);
         let list_items = [];
-        
         for (let i = 0; i < 9; i++) {
             list_items.push(document.createElement("li"));
         }
-        
+        // Adding data to HTML
         list_items[0].appendChild(document.createTextNode("Gender: " + data.Gender));
         list_items[1].appendChild(document.createTextNode("Eye color: " + data["Eye color"]));
         list_items[2].appendChild(document.createTextNode("Race: " + data.Race));
@@ -53,17 +46,15 @@ const onload = async () => {
         list_items[6].appendChild(document.createTextNode("Skin color: " + data["Skin color"]));
         list_items[7].appendChild(document.createTextNode("Alignment: " + data.Alignment));
         list_items[8].appendChild(document.createTextNode("Weight: " + data.Weight));
-        
         for (let i = 0; i < 9; i++) {
-            list.appendChild(list_items[i]);
+            new_list.appendChild(list_items[i]);
         }
-        
-        body.style.visibility = "visible";
-    } catch (error) {
-        console.log("An error occurred: " + error);
     }
+    // Unhide content
+    body.style.visibility = "visible";
 };
 
+// Method to redirect to homepage
 const home = () => {
     window.location.href = "/";
 };
