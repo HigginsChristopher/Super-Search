3c3c71bd84cd1a4d11ae40d9b439c5b1462c9022
Christopher Higgins <chiggi24@uwo.ca>
2023-11-20 17:22:39 -0500

    Created about component
    Created about component to create about page with version, name, and link back to the homepage.

--- client/app/src/app/components/about/about.component.ts
+++ client/app/src/app/components/about/about.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TitleService } from '../../services/title.service';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
-export class AboutComponent {
-
+export class AboutComponent implements OnInit{
+  constructor(private titleService: TitleService){}
+  ngOnInit(): void {
+    this.titleService.setTitle('About Page');
+  }
 }
