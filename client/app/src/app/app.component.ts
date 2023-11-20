18ca041635327435022aab2611d26fe099af3c96
Christopher Higgins <chiggi24@uwo.ca>
2023-11-20 17:05:15 -0500

    Adjusted app component
    Adjusted app component to set title on initialization, a method to route home and a method to check the current route.

--- client/app/src/app/app.component.ts
+++ client/app/src/app/app.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TitleService } from './services/title.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private titleService: TitleService, private router: Router) { }
+  
+  ngOnInit(): void {
+    this.titleService.setTitle("Homepage");
+  }
+  
+  hasRoute(route: string) {
+    return this.router.url === route;
+  }
+
+  routeHome() {
+    this.router.navigate(['/']);
+  }
 }
