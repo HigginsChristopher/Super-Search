2d0db55acc77f0a7595bf59565cd450af3747f64
Christopher Higgins <chiggi24@uwo.ca>
2023-11-20 16:59:42 -0500

    Added list service
    Created list service to allow dynamic changing of header component title for login, about us and manage lists page.

---
+++ client/app/src/app/services/title.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TitleService {
+  private titleSubject = new BehaviorSubject<string>('Homepage');
+  currentTitle$ = this.titleSubject.asObservable();
+
+  constructor(private router: Router) {
+    this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe(() => {
+      this.setTitle('Homepage');
+    });
+  }
+
+  setTitle(title: string) {
+    this.titleSubject.next(title);
+  }
+}
