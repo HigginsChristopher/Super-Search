b38b310f2cdcca79608a756dc9902099a496da68
Christopher Higgins <chiggi24@uwo.ca>
2023-11-20 17:01:29 -0500

    Update app.module.ts
    Adjusting different imports, exports and paths for the router.

--- client/app/src/app/app.module.ts
+++ client/app/src/app/app.module.ts
@@ -3,33 +3,43 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
-import { TasksComponent } from './components/tasks/tasks.component';
-import { TaskItemComponent } from './components/task-item/task-item.component';
-import { AddTaskComponent } from './components/add-task/add-task.component';
+import { ListsComponent } from './components/lists/lists.component';
+import { ListItemComponent } from './components/list-item/list-item.component';
+import { AddListComponent } from './components/add-list/add-list.component';
 import { AboutComponent } from './components/about/about.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { CreateAccountComponent } from './components/create-account/create-account.component';
+import { HeroSearchComponent } from './components/hero-search/hero-search.component';
+import { DmcaComplianceComponent } from './components/dmca-compliance/dmca-compliance.component';
 
 const appRoutes: Routes = [
-  {path: "", component: TasksComponent},
-  {path: "about", component: AboutComponent}
-
+  {path: "lists", component: ListsComponent},
+  {path: "about", component: AboutComponent},
+  { path: 'login', component: LoginComponent },
+  { path: 'create-account', component: CreateAccountComponent },
+  { path: 'hero-search', component: HeroSearchComponent },
+  { path: 'dmca-compliance', component: DmcaComplianceComponent }
 ]
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ButtonComponent,
-    TasksComponent,
-    TaskItemComponent,
-    AddTaskComponent,
+    ListsComponent,
+    ListItemComponent,
+    AddListComponent,
     AboutComponent,
-    FooterComponent
+    FooterComponent,
+    LoginComponent,
+    CreateAccountComponent,
+    HeroSearchComponent,
+    DmcaComplianceComponent,
   ],
   imports: [
     BrowserModule,
@@ -37,6 +47,7 @@ const appRoutes: Routes = [
     FontAwesomeModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
