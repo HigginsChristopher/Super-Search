1b62990220df3ac93781e563e1cca77493969584
Christopher Higgins <chiggi24@uwo.ca>
2023-11-20 17:29:10 -0500

    Updated header component
    Route different buttons to different URLs and update title.

--- client/app/src/app/components/header/header.component.ts
+++ client/app/src/app/components/header/header.component.ts
@@ -1,27 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { UiService } from '../../services/ui.service';
 import { Subscription } from 'rxjs';
 import { Router } from '@angular/router';
+import { TitleService } from '../../services/title.service';
+import { UserService } from '../../services/user.service';
+import { User } from '../../user';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
-  title: string = 'List Manager';
-  showAddTask: boolean = false;
+export class HeaderComponent implements OnInit {
+  @Input() title: string = "";
+  showAddList: boolean = false;
   subscription: Subscription = new Subscription();
+  userDetails$ = this.userService.user$;
+  user: User | null = null;
 
-  constructor(private uiService: UiService, private router: Router) {
-    this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value);
+  constructor(private userService: UserService, private uiService: UiService, private router: Router, private titleService: TitleService) {
+    this.subscription = this.uiService.onToggle().subscribe(value => this.showAddList = value);
   }
 
-  toggleAddTask() {
-    this.uiService.toggleAddTask();
+  ngOnInit(): void {
+    this.userDetails$.subscribe((user) => {
+      this.user = user;
+    });
+    this.updateTitle();
+  }
+  updateTitle(){
+    this.titleService.currentTitle$.subscribe((title) => {
+      if (this.user) {
+        if(this.hasRoute("/")){
+          this.title = `${title} - Welcome back ${this.user.username}!`;
+          console.log("here 2")
+        }
+        else{
+          this.title = title;
+        }
+      }
+      else {
+        this.title = title;
+      }
+    });
+  }
+  toggleAddList() {
+    this.uiService.toggleAddList();
   }
 
-  hasRoute(route: string){
+  hasRoute(route: string) {
     return this.router.url === route;
   }
 
+  routeLogin() {
+    const currentUrl = this.router.url;
+    const listsUrl = currentUrl.endsWith('/login') ? currentUrl : `${currentUrl}/login`;
+    this.router.navigate([listsUrl]);
+  }
+
+  routeLists() {
+    const currentUrl = this.router.url;
+    const listsUrl = currentUrl.endsWith('/lists') ? currentUrl : `${currentUrl}/lists`;
+    this.router.navigate([listsUrl]);
+  }
+
+  routeHero() {
+    const currentUrl = this.router.url;
+    const listsUrl = currentUrl.endsWith('/hero-search') ? currentUrl : `${currentUrl}/hero-search`;
+    this.router.navigate([listsUrl]);
+  }
+  routeLogout(){
+    this.user = null;
+    localStorage.setItem('currentUser', "");
+    this.updateTitle();
+  }
 }
