75289bf744d2df51233ec04fb124ee1ed5f3069e
Christopher Higgins <chiggi24@uwo.ca>
2023-11-21 18:39:10 -0500

    Update server.js
    Added authorization header to CORS middleware.
    
    Created secure and open endpoints, secure endpoints using middleware function to authenticate user based on JWT.
    
    Removed some unnecessary endpoints.

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const superhero_util = require('./script.js');
 const validator = require("validator");
 const jwt = require('jsonwebtoken');
-const crypto = require('crypto');
+//const crypto = require('crypto');
 
 // Setting up port and express application
 const port = process.env.PORT || 3000;
@@ -41,13 +41,9 @@ const authenticateToken = (req, res, next) => {
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
-
-// Middleware to server task and homepage folders for access on client
-app.use("/", express.static("../client"));
-app.use("/", express.static("../client/homepage"));
 
 // Middleware to make data in body and response is in JSON
 app.use(express.json());
@@ -72,15 +68,14 @@ app.use((req, res, next) => {
 });
 
 // ENDPOINTS
+// Endpoint to GET all list info
+app.get("/api/secure/lists", authenticateToken,(req, res) => {
+    res.send(superhero_util.get_all_lists());
+});
 
 // Authentication endpoint
-app.post('/api/login', async (req, res) => {
-    const test =
-    {
-        email: req.body.email,
-        password: req.body.password
-    };
-    const user = await superhero_util.get_user(test);
+app.post('/api/open/login', async (req, res) => {
+    const user = await superhero_util.get_user(req.body);
     if (!(user instanceof Error)) {
         // Generate JWT token
         const token = jwt.sign({ userId: user.id, email: user.email }, secretKey, { expiresIn: '1h' });
@@ -92,7 +87,7 @@ app.post('/api/login', async (req, res) => {
 });
 
 // Registration endpoint
-app.post('/api/register', async (req, res) => {
+app.post('/api/open/register', async (req, res) => {
     const user = await superhero_util.create_user(req.body);
     if (!(user instanceof Error)) {
         // Generate JWT token
@@ -104,7 +99,7 @@ app.post('/api/register', async (req, res) => {
     }
 });
 
-app.get('/api/verify', (req, res) => {
+app.get('/api/open/verify', (req, res) => {
     const user = superhero_util.verify_user(req.query.token);
     if (!(user instanceof Error)) {
         // Return a response indicating successful verification
@@ -115,23 +110,8 @@ app.get('/api/verify', (req, res) => {
     }
 });
 
-// Endpoint to GET all superhero info
-app.get("/api/superheros_info", (req, res) => {
-    res.send(superhero_util.get_all_info());
-});
-
-// Endpoint to GET all publisher info
-app.get("/api/superheros_info/publishers", (req, res) => {
-    res.send(superhero_util.get_publishers());
-});
-
-// Endpoint to GET all list info
-app.get("/api/lists", (req, res) => {
-    res.send(superhero_util.get_all_lists());
-});
-
 // Endpoint to GET all matches for a given field, query and number of results
-app.get("/api/superheros_info/match", (req, res) => {
+app.get("/api/open/superheros_info/match", (req, res) => {
     let match_result;
     // Get fields from query 
     const field = req.query.field;
@@ -161,7 +141,7 @@ app.get("/api/superheros_info/match", (req, res) => {
 });
 
 // Endpoint to GET superhero info for given id
-app.get("/api/superheros_info/:id", (req, res) => {
+app.get("/api/open/superheros_info/:id", (req, res) => {
     // Get fields from params 
     const id = Number(req.params.id);
     // Validate id (sanitize)
@@ -176,13 +156,8 @@ app.get("/api/superheros_info/:id", (req, res) => {
     res.send(superhero_info);
 });
 
-// Endpoint to GET all superhero powers
-app.get("/api/superheros_powers", (req, res) => {
-    res.send(superhero_util.get_all_powers());
-});
-
 // Endpoint to GET all superhero powers for given id
-app.get("/api/superheros_powers/:id", (req, res) => {
+app.get("/api/open/superheros_powers/:id", (req, res) => {
     // Get fields from params 
     const id = Number(req.params.id);
     // Validate id (sanitize)
@@ -197,16 +172,16 @@ app.get("/api/superheros_powers/:id", (req, res) => {
     res.send(superhero_powers);
 });
 
-// Endpoint to GET all list_id for a given list name
-app.get("/api/lists/ids", (req, res) => {
+// Endpoint to GET list_id for a given list id
+app.get("/api/open/lists/:id", (req, res) => {
     // Decode name (can have special characters)
-    const name = decodeURIComponent(req.query.name)
+    const id = req.query.id;
     // Validate name (sanitize)
-    const { error } = superhero_util.validate_name(name);
+    const { error } = superhero_util.validate_id(id);
     // Validation raised error, return error message (400 client error)
     if (error) return res.status(400).send(error.details[0].message);
     // Get list_id for given name
-    const list = superhero_util.get_list_ids(name)
+    const list = superhero_util.get_list_ids(id)
     // No name matches, return error message (404 error)
     if (list instanceof Error) return res.status(404).send(list.message);
     // Send id_list to client
@@ -214,7 +189,7 @@ app.get("/api/lists/ids", (req, res) => {
 });
 
 // Endpoint to GET all superhero and power info for a given list of ids
-app.get("/api/lists/details", (req, res) => {
+app.get("/api/open/lists/details", (req, res) => {
     let id_list;
     // Parse id_list from query
     try {
@@ -233,24 +208,19 @@ app.get("/api/lists/details", (req, res) => {
     res.send(list);
 })
 
-// Endpoint to GET list for given name
-app.get("/api/lists/:name", (req, res) => {
-    // Get name from params
-    const name = req.params.name
-    // Validate name (sanitize)
-    const { error } = superhero_util.validate_name(name);
-    // Validation raised error, return error message (400 client error)
-    if (error) return res.status(400).send(error.details[0].message);
-    // Get list for given name
-    const list = superhero_util.get_list(name)
-    // No name matches, return error message (404 error)
-    if (list instanceof Error) return res.status(404).send(list.message);
-    // Send list to client
-    res.send(list);
-});
+// Endpoint to GET all superhero and power info for a given list of ids
+app.get("/api/open/lists/", (req, res) => {
+    // Get details for given id_list
+    const lists = superhero_util.get_recent_public_lists(req.body);
+    // No id matches, return error message (404 error)
+    if (lists instanceof Error) return res.status(404).send(lists.message);
+    // Send details to client
+    res.send(lists);
+})
+
 
 // Endpoint to POST, in this case create, list for given name and id_list
-app.post("/api/lists", (req, res) => {
+app.post("/api/secure/lists", authenticateToken,(req, res) => {
     // Validate create list details (sanitize)
     const { error } = superhero_util.validate_create_list(req.body);
     if (error) return res.status(400).send(error.details[0].message);
@@ -267,9 +237,9 @@ app.post("/api/lists", (req, res) => {
 })
 
 // Route to handle /api/lists/:id with a given id parameter
-app.route("/api/lists/:id")
+app.route("/api/secure/lists/:id")
     // Endpoint to POST, in this case update, a list with the given id and id_list
-    .post((req, res) => {
+    .post(authenticateToken,(req, res) => {
         const list_object = {
             // Get id from params
             id: req.params.id,
@@ -290,7 +260,7 @@ app.route("/api/lists/:id")
         res.send(list);
     })
     // Endpoint to DELETE a list with the given id and id_list
-    .delete((req, res) => {
+    .delete(authenticateToken, (req, res) => {
         // Get id from params
         const id = req.params.id
         // Validate id (sanitize)
