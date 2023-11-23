88a99069eff9a62172c248681c85ab3f31d9dd3b
Christopher Higgins <chiggi24@uwo.ca>
2023-11-23 14:32:51 -0500

    Update script.js
    Adjusted methods to get powers based on id and name, changed match method using fuse to work on multiple filters, adjusted get list, save list, delete list and create list to work with multiple users, adjusted get details from list method to work, and finally adjusted get all info method to return all superhero info (info and powers) all at once.

--- server/script.js
+++ server/script.js
@@ -4,6 +4,8 @@ const Joi = require("joi");
 const FileSync = require("lowdb/adapters/FileSync");
 const sanitize_html = require("sanitize-html");
 const crypto = require("crypto");
+const Fuse = require('fuse.js');
+const bcrypt = require('bcrypt');
 
 // Superhero info database and syncing it with an adapter
 const info_adapter = new FileSync("../data/superhero_info.json");
@@ -35,13 +37,7 @@ const id_db = low(id_adapter)
 id_db.read();
 
 // Default if ids.json doesn't exists
-id_db.defaults({ "highestUserId": -1 }).write();
-
-// Method to get all superhero info
-const get_all_info = () => info_db.get("content").value();
-
-const bcrypt = require('bcrypt');
-const { time } = require("console");
+id_db.defaults({ "highestUserId": -1, "highestListId": -1 }).write();
 
 const saltRounds = 10; // Salt rounds for bcrypt (adjust as needed)
 
@@ -70,7 +66,7 @@ const get_user = async (user) => {
     const approve = await comparePasswords(user.password, info.password)
     // Return error if no result for given id
     if (!approve) return new Error(`Wrong password.`)
-    if (!info.activated) return new Error(`Account not enabled! Ensure email is verified, otherwise contact an adminstrator.`)
+    if (!info.activated) return new Error(`Account not verified! Check your email for verification email.`)
     return info;
 }
 
@@ -86,7 +82,7 @@ const create_user = async (user) => {
     const newHighestId = currentHighestId + 1;
     id_db.set('highestUserId', newHighestId)
         .write();
-    const newUser = { "id": currentHighestId, "username": user.username, "password": hash, "email": user.email, "verificationToken": verificationToken, activated: false };
+    const newUser = { "id": newHighestId, "username": user.username, "password": hash, "email": user.email, "verificationToken": verificationToken, activated: false };
     user_db.get("users").push(newUser).write();
     return newUser;
 }
@@ -100,7 +96,7 @@ const verify_user = token => {
 }
 
 // Method to get superhero info for a specified id
-const get_info = id => {
+const get_info_id = id => {
     const info = info_db.get("content").find({ "id": id }).value();
     // Return error if no result for given id
     if (info === undefined) return new Error(`No superhero info for given ID: ${id}`)
@@ -108,23 +104,39 @@ const get_info = id => {
 }
 
 // Method to get superhero id for a specified name
-const get_id = name => {
+const get_info_name = name => {
     const info = info_db.get("content").find({ "name": name }).value();
     // Return error if no result for given name
     if (info === undefined) return new Error(`No superhero info for given name: ${name}`)
     return info.id;
 }
 
-// Method to get all superhero powers
-const get_all_powers = () => powers_db.get("content").value();
 
 // Method to get superhero powers for a specified id
-const get_powers = id => {
-    const name = get_info(id).name
-    const powers = powers_db.get("content").find({ "hero_names": name }).value();
+const get_powers_id = id => {
+    const name = get_info_id(id).name
+    const powers = structuredClone(powers_db.get("content").find({ "hero_names": name }).value());
     // Return error if no result for given name
-    if (powers === undefined) return new Error(`No powers for ${name} - ID: ${id}`);
-    return powers;
+    if (powers === undefined) return {};
+    delete powers.hero_names;
+    // Filter out powers that are not true
+    const filteredPowers = Object.fromEntries(
+        Object.entries(powers).filter(([key, value]) => value === "True")
+    );
+    return filteredPowers;
+}
+
+// Method to get superhero powers for a specified id
+const get_powers_name = name => {
+    const powers = structuredClone(powers_db.get("content").find({ "hero_names": name }).value());
+    // Return powers for given name
+    if (powers === undefined) return {};
+    delete powers.hero_names;
+    // Filter out powers that are not true
+    const filteredPowers = Object.fromEntries(
+        Object.entries(powers).filter(([key, value]) => value === "True")
+    );
+    return filteredPowers;
 }
 
 // Method to get all publisher names
@@ -141,52 +153,28 @@ const get_publishers = () => {
 }
 
 // Method to get n results for a given field and match
-const match = (field, match, n = 734) => {
-    let match_result = [];
-    // Matching for power field is unique (user must enter the correct power name, case-insensitive)
-    if (field === "power") {
-        // Capitalizing all word first letters and lower casing rest
-        match = capitalize_words(match);
-        const superhero_info = powers_db.get("content");
-        for (const superhero of superhero_info) {
-            // If one superhero doesn't have power, return no power error
-            if (superhero[match] === undefined) {
-                return new Error(`No power called '${match}'!`);
-            }
-            // If power is true and there can be more results
-            else if (superhero[match] === "True" && match_result.length < n) {
-                const id = get_id(superhero.hero_names)
-                // If result is not instance of error, push it to results
-                if (!(id instanceof Error)) {
-                    match_result.push(id);
-                }
-            }
-        }
-    }
-    // Matching for other fields
-    else {
-        const superhero_info = info_db.get("content");
-        const lower_case_field = field.toLowerCase();
-        const lower_case_match = match.toLowerCase();
-        // Filter results for given field and name taking only n ids 
-        match_result = superhero_info
-            .filter(item => {
-                const fieldValue = String(item[field]);
-                return fieldValue.toLowerCase().includes(lower_case_match);
-            })
-            .take(n)
-            .map("id")
-            .value();
-        // If result is empty, no result for given field and match
-        if (match_result.length === 0) return new Error(`No results for field: '${field}' and match: '${match}'!`);
-    }
-    return match_result;
+const match = (filters, limit = 734) => {
+    // Remove properties with empty values
+    const nonEmptyFilters = Object.fromEntries(
+        Object.entries(filters).filter(([key, value]) => value !== '')
+    );
+    const heroes = get_all_info();
+
+    const options = {
+        keys: ['name', "Race", ['powers'], 'Publisher'],
+        includeScore: true,
+        findAllMatches: true,
+    };
+
+    const fuse = new Fuse(heroes, options);
+    const results = fuse.search(nonEmptyFilters);
+    const limitedResults = results.slice(0, limit);
+    return limitedResults.map(result => result.item);
 }
 
 // Method to get all lists
 const get_lists = (id) => {
     const lists = list_db.get("lists");
-
     result = lists
         .filter(list => {
             return list.user_id == id
@@ -202,37 +190,41 @@ const get_lists = (id) => {
 const get_list_name = (name, user_id) => {
     const list = list_db.get("lists").find(list => list["list-name"].toLowerCase() == name.toLowerCase() && list.user_id == user_id);
     // Return error if no result for given name
-    if (list.value() === undefined) return new Error(`No list result for given name: ${name}`);
+    if (list.value() === undefined) return new Error(`No list result for user with given list name: ${name}`);
     return list;
 }
 
 // Method to get list for a given id
-const get_list_id = (id) => {
-    const list = list_db.get("lists").find(list => list.id == id);
+const get_list_id = (list_id, user_id) => {
+    const list = list_db.get("lists").find(list => list.list_id === list_id && list.user_id === user_id);
     // Return error if no result for given id
-    if (list.value() === undefined) return new Error(`No list result for given ID: ${id}`);
+    if (list.value() === undefined) return new Error(`No list result for user with given list id: ${list_id}`);
     return list;
 }
 
 // Method to create list with a given name and id_list
-const create_list = (user_id, name, id_list, description, visibility) => {
+const create_list = (list_object) => {
     // Check if list already exists
-    const list = get_list_name(name,user_id);
+    const list = get_list_name(list_object["list-name"], list_object.user_id);
     // List doesn't exist, create list
     if (list instanceof Error) {
         const lists = list_db.get("lists");
 
         result = lists
             .filter(list => {
-                return list.user_id == user_id
+                return list.user_id == list_object.user_id
             })
             .value();
         if (result.length == 20) {
             return new Error("Users can only have 20 lists!");
         }
+        const currentHighestId = id_db.get('highestListId').value();
+        const newHighestId = currentHighestId + 1;
+        id_db.set('highestListId', newHighestId)
+            .write();
         const timestamp = Date.now();
         // Create list and write to database
-        const newList = { "user_id": user_id, "list-name": name, "superhero_ids": id_list, "description": description, "visibility": visibility, "modified": timestamp };
+        const newList = { "user_id": list_object.user_id, "list_id": newHighestId, "list-name": list_object["list-name"], "superhero_ids": list_object.id_list, "description": list_object.description, "visibility": list_object.visibility, "modified": timestamp };
         list_db.get("lists").push(newList).write();
         return newList;
     }
@@ -245,15 +237,15 @@ const create_list = (user_id, name, id_list, description, visibility) => {
 // Method to save list with a given id and id_list
 const save_list = (list_object) => {
     // Check if list already exists
-    const list = get_list_id(list_object.id);
+    const list = get_list_id(list_object.list_id, list_object.user_id);
     // List exists, save list
     if (!(list instanceof Error)) {
         const timestamp = Date.now();
-        list.assign({ "list-name": list_object["list-name"], "superhero_ids": list_object.superhero_ids, "description": list_object.description, "visibility": list_object.visibility, "modified": timestamp }).write();
+        list.assign({ "user_id": list_object.user_id, "list_object": list_object.list_id, "list-name": list_object["list-name"], "superhero_ids": list_object.superhero_ids, "description": list_object.description, "visibility": list_object.visibility, "modified": timestamp }).write();
     }
     // List with given ID doesn't exist, return error
     else {
-        return new Error("List with given ID doesn't exist!");
+        return new Error("List with given list-ID doesn't exist!");
     }
 };
 
@@ -272,30 +264,31 @@ const get_list_ids = (id) => {
 };
 
 // Method to delete list for a given id
-const delete_list = (id) => {
+const delete_list = (user_id, name) => {
     // Check if list already exists
-    const list = get_list_id(id);
+    const list = get_list_name(name, user_id);
     // List exists, remove list
     if (!(list instanceof Error)) {
         list_db.get("lists").remove(list.value()).write();
     }
-    // List with given ID doesn't exist, return error
     else {
-        return new Error("List with given ID doesn't exist!");
+        return list;
     }
 };
 
 // Method to get details from a given id_list
 const get_details_from_list = (id_list) => {
     // No detail duplicates (set)
-    let results = new Set();
+    let results = []
     for (let i = 0; i < id_list.length; i++) {
-        let info = get_info(id_list[i]);
-        info.powers = get_powers(id_list[i]);
-        results.add(info);
+        const hero = get_info_id(id_list[i]);
+        hero.powers = get_powers_id(id_list[i]);
+        hero.powers = hero.powers ? Object.keys(hero.powers).filter(power => hero.powers[power]) : [];
+        results.push(hero);
     }
     // Convert back to array
-    return Array.from(results);
+    return results;
+
 }
 
 const get_recent_public_lists = () => {
@@ -313,7 +306,8 @@ const get_recent_public_lists = () => {
 // Method to check validity of update list details
 const validate_update_list = (list) => {
     const schema = {
-        id: Joi.number().integer().max(733).min(0).required(),
+        user_id: Joi.number().integer().max(733).min(0).required(),
+        list_id: Joi.number().integer().max(733).min(0).required(),
         ["list-name"]: Joi.string().max(100).min(1).required(),
         superhero_ids: Joi.array().items(Joi.number().integer().min(0).max(734)).unique().strict().required(),
         description: Joi.string().allow('').max(1000).min(0).strict().required(),
@@ -347,13 +341,13 @@ const validate_id = (id) => {
 
 // Method to check validity of list names
 const validate_name = (name) => {
-    const superhero_object = {
+    const list_object = {
         name: name
     }
     const schema = {
         name: Joi.string().max(100).min(1).required()
     }
-    return Joi.validate(superhero_object, schema);
+    return Joi.validate(list_object, schema);
 };
 
 // Method to check validity of id_lists inside list
@@ -362,7 +356,7 @@ const validate_id_list = (id_list) => {
         id_list: id_list
     }
     const schema = {
-        id_list: Joi.array().items(Joi.number().integer()).max(734).min(0).required()
+        id_list: Joi.array().items(Joi.number().integer()).max(734).min(0).unique().required()
     }
     return Joi.validate(superhero_object, schema);
 };
@@ -397,13 +391,22 @@ const sanitize_user_input = (obj) => {
     }
 };
 
-// Method to capitalize first letter of every word in a given sentence and make all other characters lowercase
-const capitalize_words = (str) => {
-    const words = str.trim().split(/\s+/);
-    const capitalized_words = words.map(word => {
-        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+// Method to get all superhero info
+const get_all_info = () => {
+    const superheroes = info_db.get("content").value();
+    for (const superhero of superheroes) {
+        const powers = get_powers_name(superhero.name);
+        superhero.powers = powers;
+    }
+    const flattenedHeroes = superheroes.map(hero => {
+        const powersArray = hero.powers ? Object.keys(hero.powers).filter(power => hero.powers[power]) : [];
+        return {
+            ...hero,
+            powers: powersArray,
+        };
     });
-    return capitalized_words.join(' ');
+    
+    return flattenedHeroes;
 };
 
 // Exporting methods for usage inside server.js
@@ -424,9 +427,9 @@ module.exports = {
     delete_list,
     get_details_from_list,
     get_all_info,
-    get_info,
-    get_all_powers,
-    get_powers,
+    get_info_name,
+    get_info_id,
+    get_powers_id,
     get_publishers,
     match,
     hashPassword,
