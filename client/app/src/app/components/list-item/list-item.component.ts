4abe1610941530ba924c7c400a4a2a23041229c5
Christopher Higgins <chiggi24@uwo.ca>
2023-11-28 14:39:04 -0500

    Updated list-item component
    Updated list-item component to handle different modals (error handling, creating review and list modification).
    
    Updated oninit to display correct content the list headers.
    
    Updated toggleexpand to show reviews.
    
    Emitted alot of the functionality so that list could update the contents on screen.

--- client/app/src/app/components/list-item/list-item.component.ts
+++ client/app/src/app/components/list-item/list-item.component.ts
@@ -4,7 +4,12 @@ import { faTimes } from "@fortawesome/free-solid-svg-icons"
 import { Superhero } from '../../superhero';
 import { SuperheroService } from '../../services/superhero.service';
 import { ListService } from '../../services/list.service';
-
+import { User } from '../../user';
+import { UserService } from '../../services/user.service';
+import { HeroService } from '../../services/hero.service';
+import { ReviewService } from '../../services/review.service';
+import { Review } from '../../Review';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-list-item',
@@ -14,14 +19,30 @@ import { ListService } from '../../services/list.service';
 export class ListItemComponent implements OnInit {
   faTimes = faTimes;
   @Input() list!: List;
+  @Input() listType!: string;
   @Output() onDeleteList: EventEmitter<List> = new EventEmitter();
   @Output() onToggleList: EventEmitter<List> = new EventEmitter();
-  superheroes: Superhero[] = [];
+  @Output() onUpdateList: EventEmitter<List> = new EventEmitter();
+  @Output() onCreateReview: EventEmitter<Review> = new EventEmitter();
 
+
+  superheroes: Superhero[] = [];
+  reviews: Review[] = [];
+  currentUser: User | null = null;
+  formattedDateTime!: string;
+  selectedSuperhero: Superhero | null = null;
+  selectedList: List | null = null;
+  selectedReview: List | null = null;
   expanded: boolean = false;
   private longPressTimer: any;
+  showErrorPopup: boolean = false;
+  errorMessages: string = '';
 
-  constructor(private superheroService: SuperheroService, private listService: ListService) { }
+  constructor(
+    private listService: ListService,
+    private userService: UserService,
+    private heroService: HeroService,
+    private reviewService: ReviewService) { }
 
   @HostListener('mouseup') onMouseUp() {
     this.clearLongPressTimer();
@@ -32,7 +53,7 @@ export class ListItemComponent implements OnInit {
   }
 
 
-  startLongPressTimer(event: MouseEvent,list: List): void {
+  startLongPressTimer(event: MouseEvent, list: List): void {
     if (event.button === 0) { // Check left mouse button
       this.longPressTimer = setTimeout(() => {
         this.onToggleList.emit(list);
@@ -47,10 +68,47 @@ export class ListItemComponent implements OnInit {
 
   toggleExpand(list: List): void {
     this.expanded = !this.expanded;
-    this.listService.displayDetails(list).subscribe((superheroes) => this.superheroes = superheroes);
+    this.heroService.displayDetails(list.superhero_ids).subscribe((superheroes) => this.superheroes = superheroes);
+    this.reviewService.displayReviews(list.list_id).subscribe((reviews) => {
+      this.reviews = reviews;
+      if (this.reviews.length > 0) {
+        for (let review of this.reviews) {
+          this.userService.getUserName(review.user_id).subscribe(response => {
+            review.username = response.username
+          });
+        }
+      }
+    });
   }
 
   ngOnInit(): void {
+    this.userService.getCurrentUser().subscribe(user => {
+      this.currentUser = user;
+    });
+    this.reviewService.displayReviews(this.list.list_id).subscribe((reviews) => {
+      if (Array.isArray(reviews)) {
+        const totalRatings = reviews.reduce((sum, review) => sum + review.rating, 0);
+        this.list.rating = totalRatings / reviews.length;
+      } else {
+        this.list.rating = "No reviews"
+      }
+      // Create a new Date object and pass the timestamp as an argument
+      const date = new Date(this.list.modified);
+
+      // Use various methods of the Date object to extract different components of the date
+      const year = date.getFullYear();
+      const month = date.getMonth() + 1; // Months are zero-based, so add 1
+      const day = date.getDate();
+      const hours = date.getHours().toString().padStart(2, '0'); // Ensure leading zero
+      const minutes = date.getMinutes().toString().padStart(2, '0'); // Ensure leading zero
+      const seconds = date.getSeconds().toString().padStart(2, '0'); // Ensure leading zero
+
+      // Format the components using toLocaleString
+      this.formattedDateTime = `${month}/${day}/${year} ${hours}:${minutes}:${seconds}`;
+    });
+    this.userService.getUserName(this.list.user_id).subscribe(response => {
+      this.list.username = response.username
+    });
   }
 
   onDelete(list: List) {
@@ -61,11 +119,44 @@ export class ListItemComponent implements OnInit {
     this.onToggleList.emit(list);
   }
 
-  onDeleteSuperhero(list: List, superhero: Superhero){
-    const indexToDelete = list.superhero_ids.indexOf(superhero.id);
-    list.superhero_ids.splice(indexToDelete,1)
-    this.listService.updateList(list).subscribe();
-    this.listService.displayDetails(list).subscribe((superheroes) => this.superheroes = superheroes);
+  updateList(list: List): void {
+    this.onUpdateList.emit(list);
+    this.toggleExpand(list);
   }
 
+  createReview(review: Review): void {
+    this.onCreateReview.emit(review);
+  }
+
+  openSuperheroModal(superhero: any): void {
+    this.selectedSuperhero = superhero;
+  }
+
+  closeSuperheroModal(): void {
+    this.selectedSuperhero = null;
+  }
+
+  openListModificationModal(list: any): void {
+    this.selectedList = list;
+  }
+
+  closeListModificationModal(): void {
+    this.selectedList = null;
+  }
+
+  openCreateReviewModal(list: List): void {
+    this.selectedReview = list;
+  }
+
+  closeCreateReviewModal(): void {
+    this.selectedReview = null;
+  }
+
+  showPopupWithError(message: any) {
+    this.errorMessages = message;
+    this.showErrorPopup = true;
+  }
+  onClosePopup() {
+    this.showErrorPopup = false;
+  }
 }
