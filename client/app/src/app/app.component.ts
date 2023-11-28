890ab05cec4115fe6970f4b9e2d1168e05a9a16c
Christopher Higgins <chiggi24@uwo.ca>
2023-11-28 14:16:02 -0500

    Update app componenet
    Added auto logout for token expiry and login/logout function to help with maintaining user data between sessions.

--- client/app/src/app/app.component.ts
+++ client/app/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { TitleService } from './services/title.service';
 import { Router } from '@angular/router';
+import { UserService } from './services/user.service';
+import { jwtDecode } from 'jwt-decode';
 
 @Component({
   selector: 'app-root',
@@ -8,12 +10,44 @@ import { Router } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
-  constructor(private titleService: TitleService, private router: Router) { }
-  
+  constructor(private titleService: TitleService, private router: Router, private userService: UserService) { }
+
   ngOnInit(): void {
     this.titleService.setTitle("Homepage");
+
+    const token = JSON.parse(localStorage.getItem('token')!);
+    const currentUser = JSON.parse(localStorage.getItem('userData')!);
+
+    if (!token || !currentUser) {
+      return;
+    }
+
+    // Check if the token is expired
+    const isTokenExpired = this.isTokenExpired(token);
+
+    if (isTokenExpired) {
+      // Token is expired, log out the user
+      this.logout();
+    } else {
+      // Token is not expired, set the user
+      this.userService.setUser(currentUser);
+    }
   }
-  
+
+  isTokenExpired(token: string): boolean {
+    const decodedToken: any = jwtDecode(token);
+
+    // Check if the current time is greater than the token's expiration time
+    return decodedToken.exp * 1000 < Date.now();
+  }
+
+  logout() {
+    this.userService.setUser(null);
+    localStorage.removeItem('token');
+    localStorage.removeItem('userData');
+    this.routeHome();
+  }
+
   hasRoute(route: string) {
     return this.router.url === route;
   }
