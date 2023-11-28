3e0e582efd8165ddc5a504bbc312cac101cebda4
Christopher Higgins <chiggi24@uwo.ca>
2023-11-28 14:23:38 -0500

    Updated create account component
    Updated create account component to display error messages on error message modal, with ability to close popup. Also added function to verify user, and redirect them to the homepage, logged in.
    
    Created some utility methods to decode jwt and get errors from forms.
    
    Added error popup component to html.

--- client/app/src/app/components/create-account/create-account.component.ts
+++ client/app/src/app/components/create-account/create-account.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { TitleService } from '../../services/title.service';
-import { Router } from '@angular/router';
 import { UserService } from '../../services/user.service';
 import { User } from '../../user';
+import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+import { faL } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-create-account',
@@ -15,6 +17,15 @@ export class CreateAccountComponent implements OnInit {
   registrationSuccess = false;
   createdUser: User | null = null;
   mockEmail = "";
+  verifyUrl = "";
+  showErrorPopup: boolean = false;
+  errorMessages: string = '';
+
+  // Call this function when you want to show the error popup
+  showPopupWithError(message: any) {
+    this.errorMessages = message;
+    this.showErrorPopup = true;
+  }
 
   constructor(
     private formBuilder: FormBuilder,
@@ -30,17 +41,15 @@ export class CreateAccountComponent implements OnInit {
   }
 
   onSubmit() {
-    const email = this.createAccountForm.get('email')?.value;
-    const password = this.createAccountForm.get('password')?.value;
-    const nickname = this.createAccountForm.get('nickname')?.value;
-
-    if (email !== null && password !== null && nickname !== null) {
-      console.log("here");
+    if (this.createAccountForm.valid) {
+      const email = this.createAccountForm.get('email')?.value;
+      const password = this.createAccountForm.get('password')?.value;
+      const nickname = this.createAccountForm.get('nickname')?.value;
       const newUser: User = {
         username: nickname,
         password: password,
         email: email,
-        activated: false,
+        verified: false,
       };
       this.userService.registerUser(newUser).subscribe(
         (response: any) => {
@@ -48,34 +57,87 @@ export class CreateAccountComponent implements OnInit {
           this.registrationSuccess = true;
           this.createMockEmail();
         },
-        (error) => {
-          console.error('Error creating user:', error);
+        (error: any) => {
+          if (error instanceof HttpErrorResponse) {
+            // Check for specific HTTP error status codes and handle them
+            this.showPopupWithError(error.error.message);
+          } else {
+            // Handle non-HTTP errors or display a generic error message
+            this.showPopupWithError('An unexpected error occurred.');
+          }
         }
       );
-    } else {
-      // Form is invalid, display error messages or handle accordingly
     }
+    else {
+      this.showErrorPopup = true;
+      this.showPopupWithError(this.getFormValidationErrors());
+    }
+  };
+
+  onClosePopup() {
+    this.showErrorPopup = false;
   }
 
   ngOnInit(): void {
     this.titleService.setTitle('Create Account');
   }
 
-  resendVerification() {
-    if (this.createdUser) {
-      this.createMockEmail("Resent account confirmation. ")
-    }
-  }
+  createMockEmail(): void {
+    const protocol = window.location.protocol;
+    const host = window.location.host;
 
-  createMockEmail(string=""): void {
-    const url = `http://localhost:3000/api/open/verify?token=${this.createdUser?.verificationToken}`
+    const baseUrl = `${protocol}//${host}`;
+    const url = `${baseUrl}/api/open/users/verify?token=${this.createdUser?.verificationToken}`
     this.mockEmail = `
       Mock Email:
       From: Do Not Reply <myEmail@myService.myDomain>
       To: <${this.createdUser?.email}>
-      Subject: ${string}Please confirm account for SE3316 Lab 4
+      Subject: Please confirm account for SE3316 Lab 4
       Text: Please confirm your account by clicking the following link: 
-      ${url}
     `
+    this.verifyUrl = url;
+  }
+  onVerify() {
+    this.userService.verifyUser(this.createdUser!.verificationToken!).subscribe(response => {
+      localStorage.setItem('token', JSON.stringify(response.token));
+      const user = this.decodeJwt(response.token);
+      localStorage.setItem('userData', JSON.stringify(user));
+      this.userService.setUser(user);
+      this.router.navigate(['/'])
+    });
+  }
+
+  decodeJwt(token: string) {
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const jsonPayload = decodeURIComponent(
+      atob(base64)
+        .split('')
+        .map(function (c) {
+          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        })
+        .join('')
+    );
+    return JSON.parse(jsonPayload);
+  }
+
+  getFormValidationErrors(): string[] {
+    const errorMessages: string[] = [];
+    const errorMapping: { [key: string]: string } = {
+      required: 'is required',
+      email: 'must be a valid email address'
+    };
+
+    Object.keys(this.createAccountForm.controls).forEach(key => {
+      const controlErrors = this.createAccountForm.get(key)!.errors;
+      if (controlErrors != null) {
+        Object.keys(controlErrors).forEach(keyError => {
+          const message = `${key.charAt(0).toUpperCase() + key.slice(1)} ${errorMapping[keyError as keyof typeof errorMapping]}`;
+          errorMessages.push(message);
+        });
+      }
+    });
+    return errorMessages;
   }
 }
+
