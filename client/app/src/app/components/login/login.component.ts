f2a5b2dc596d5858bfde6f3f7c6ad943b097f588
Christopher Higgins <chiggi24@uwo.ca>
2023-11-28 14:50:48 -0500

    Update login component
    Updated login component to use error popup component.

--- client/app/src/app/components/login/login.component.ts
+++ client/app/src/app/components/login/login.component.ts
@@ -3,9 +3,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { TitleService } from '../../services/title.service';
 import { UserService } from '../../services/user.service';
-import { User } from '../../user';
 import { Router } from '@angular/router';
-
+import { HttpErrorResponse } from '@angular/common/http';
+import { User } from '../../user';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -13,7 +13,9 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
-  user: User | null = null;
+  showErrorPopup: boolean = false;
+  errorMessages: string = '';
+  currentUser: User | null = null;
 
 
   constructor(
@@ -42,17 +44,73 @@ export class LoginComponent implements OnInit {
       }
       this.userService.loginUser(user).subscribe(
         (response: any) => {
-          const currentUser = response.user;
-          this.user = response.user;
-          localStorage.setItem('currentUser', JSON.stringify({currentUser}));
-          this.userService.setUser(this.user);
-          this.router.navigate(['/']);
+          localStorage.setItem('token', JSON.stringify(response.token));
+          const user = this.decodeJwt(response.token);
+          localStorage.setItem('userData', JSON.stringify(user));
+          this.userService.setUser(user);
+          this.router.navigate(['/'])
         },
-        (error) => {
-          console.error('Error logging into user:', error);
+        (error: any) => {
+          if (error instanceof HttpErrorResponse) {
+            // Check for specific HTTP error status codes and handle them
+            this.currentUser = {
+              email: this.loginForm.get('email')?.value,
+              password: this.loginForm.get('password')?.value
+            }
+            this.showPopupWithError(error.error.message);
+          } else {
+            // Handle non-HTTP errors or display a generic error message
+            this.showPopupWithError('An unexpected error occurred.');
+          }
         }
       );
     } else {
+      this.showErrorPopup = true;
+      this.showPopupWithError(this.getFormValidationErrors());
     }
   }
+
+  // Call this function when you want to show the error popup
+  showPopupWithError(message: any) {
+    this.errorMessages = message;
+    this.showErrorPopup = true;
+  }
+
+  decodeJwt(token: string) {
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const jsonPayload = decodeURIComponent(
+      atob(base64)
+        .split('')
+        .map(function (c) {
+          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        })
+        .join('')
+    );
+    return JSON.parse(jsonPayload);
+  }
+
+  getFormValidationErrors(): string[] {
+    const errorMessages: string[] = [];
+    const errorMapping: { [key: string]: string } = {
+      required: 'is required',
+      email: 'must be a valid email address'
+    };
+
+    Object.keys(this.loginForm.controls).forEach(key => {
+      const controlErrors = this.loginForm.get(key)!.errors;
+      if (controlErrors != null) {
+        Object.keys(controlErrors).forEach(keyError => {
+          const message = `${key.charAt(0).toUpperCase() + key.slice(1)} ${errorMapping[keyError as keyof typeof errorMapping]}`;
+          errorMessages.push(message);
+        });
+      }
+    });
+    return errorMessages;
+  }
+
+  onClosePopup() {
+    this.showErrorPopup = false;
+  }
+
 }
