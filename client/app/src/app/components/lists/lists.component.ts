76e09beaebb8b5200e72138435a1dfb03c3eab7c
Christopher Higgins <chiggi24@uwo.ca>
2023-11-28 14:46:43 -0500

    Update lists component
    Updated lists component to handle emitted events and refresh content on screen. Lists handles error popup, addlist, createreview, updatelist, toggelist events from list-item and modals inside list-item.
    
    Updated oninit function to assign private and public lists.

--- client/app/src/app/components/lists/lists.component.ts
+++ client/app/src/app/components/lists/lists.component.ts
@@ -2,6 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { ListService } from "../../services/list.service"
 import { List } from "../../List";
 import { TitleService } from '../../services/title.service';
+import { UserService } from '../../services/user.service';
+import { User } from '../../user';
+import { HttpErrorResponse } from '@angular/common/http';
+import { HeroService } from '../../services/hero.service';
+import { ReviewService } from '../../services/review.service';
+import { Review } from '../../Review';
 
 @Component({
   selector: 'app-lists',
@@ -9,33 +15,94 @@ import { TitleService } from '../../services/title.service';
   styleUrl: './lists.component.css'
 })
 export class ListsComponent implements OnInit {
-  lists: List[] = [];
-  constructor(private listService: ListService, private titleService: TitleService) { }
+  privateLists: List[] = [];
+  publicLists: List[] = [];
+  currentUser: User | null = null;
+  showErrorPopup: boolean = false;
+  errorMessages: string = '';
+
+  constructor(
+    private listService: ListService,
+    private titleService: TitleService,
+    private userService: UserService,
+    private heroService: HeroService,
+    private reviewService: ReviewService
+  ) {
+  }
 
   ngOnInit(): void {
+    this.userService.getCurrentUser().subscribe(user => {
+      this.currentUser = user;
+    });
     this.titleService.setTitle('List Manager');
-    this.listService.getLists().subscribe((lists) => this.lists = lists);
+    if (this.currentUser) {
+      this.listService.getPrivateLists().subscribe((lists) => this.privateLists = lists);
+    }
+    this.listService.getPublicLists().subscribe((lists) => this.publicLists = lists);
   }
 
-  deleteList(list: List) {
-    this.listService.deleteList(list).subscribe(() => this.lists = this.lists.filter(l => l.id !== list.id));
+  deleteList(list: any) {
+    this.listService.deletePrivateList(list).subscribe(() => this.privateLists = this.privateLists.filter(l => l.list_id !== list.list_id));
+    this.ngOnInit();
   }
 
-  toggleList(list: List) {
+  toggleList(list: any) {
     list.visibility = !list.visibility;
     this.updateList(list);
   }
-  
-  updateList(list: List){
-    this.listService.updateList(list).subscribe();
+
+  updateList(list: List) {
+    this.listService.updateList(list).subscribe((response: any) => {
+      if (response?.message) {
+        this.showPopupWithError(response.message);
+      }
+    },
+      (error: any) => {
+        if (error instanceof HttpErrorResponse) {
+          // Check for specific HTTP error status codes and handle them
+          this.showPopupWithError(error.error);
+        } else {
+          // Handle non-HTTP errors or display a generic error message
+          this.showPopupWithError('An unexpected error occurred.');
+        }
+      });
+    this.ngOnInit();
   }
 
-  addList(list: List) {
-    this.listService.addList(list).subscribe((res) => this.lists.push(res));
+  createReview(review: Review) {
+    this.reviewService.createReview(review).subscribe(
+      (response: any) => {
+        if (response.message) {
+          this.showPopupWithError(response.message);
+        }
+      },
+      (error: any) => {
+        if (error instanceof HttpErrorResponse) {
+          // Check for specific HTTP error status codes and handle them
+          this.showPopupWithError(error.error.message);
+        } else {
+          // Handle non-HTTP errors or display a generic error message
+          this.showPopupWithError('An unexpected error occurred.');
+        }
+      }
+    );
   }
 
-  displayDetails(list: List){
-    this.listService.displayDetails(list).subscribe((res) => console.log(res));
+  showPopupWithError(message: any) {
+    this.errorMessages = message;
+    this.showErrorPopup = true;
+  }
+  onClosePopup() {
+    this.showErrorPopup = false;
   }
 
+  addList(list: any) {
+    this.listService.addList(list).subscribe((res) => {
+      if (res.visibility) {
+        this.publicLists.push(res)
+      }
+      this.privateLists.push(res)
+    });
+    this.ngOnInit();
+  }
 }
