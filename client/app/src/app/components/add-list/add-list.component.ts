2cce3acafde3e8dd4809d26c3d487f05368b7768
Christopher Higgins <chiggi24@uwo.ca>
2023-11-28 14:18:11 -0500

    Updated add list component
    Made add list use form builder for easier use of form data.
    
    Added messages below input to show what is required.
    
    Adjusted input type for description to be fixed text area.

--- client/app/src/app/components/add-list/add-list.component.ts
+++ client/app/src/app/components/add-list/add-list.component.ts
@@ -1,50 +1,61 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';  // Import necessary modules
 import { List } from '../../List';
 import { UiService } from '../../services/ui.service';
 import { Subscription } from 'rxjs/internal/Subscription';
+import { UserService } from '../../services/user.service';
+import { User } from '../../user';
 
 @Component({
   selector: 'app-add-list',
   templateUrl: './add-list.component.html',
-  styleUrl: './add-list.component.css'
+  styleUrls: ['./add-list.component.css']
 })
-export class AddListComponent {
+export class AddListComponent implements OnInit {
   @Output() onAddList: EventEmitter<List> = new EventEmitter();
-  list_name: string = "";
-  superhero_ids: Array<number> = [];
-  description: string = "";
-  visibility: boolean = false;
+  listForm: FormGroup;  
   showAddList: boolean = false;
   subscription: Subscription = new Subscription();
+  currentUser: User | null = null;
 
-  constructor(private uiService: UiService) {
+  constructor(private uiService: UiService, private userService: UserService, private fb: FormBuilder) {
     this.subscription = this.uiService.onToggle().subscribe((value) => (this.showAddList = value));
+
+    // Initialize the form with FormBuilder
+    this.listForm = this.fb.group({
+      list_name: ['', Validators.required],
+      superhero_ids: ['', Validators.required],
+      description: [''],
+      visibility: [false],
+    });
+  }
+
+  ngOnInit(): void {
+    this.userService.getCurrentUser().subscribe(user => {
+      this.currentUser = user;
+    });
   }
 
   onSubmit() {
-    if (!this.list_name) {
-      alert("Please add a name!")
+    if (this.listForm.invalid) {
+      alert("Please fill out the required fields!");
       return;
     }
-    else if (!this.superhero_ids) {
-      alert("Please add a list of ids!")
-      return;
-    }
-    try {
-      this.superhero_ids = JSON.parse(`[${this.superhero_ids}]`)
-    } catch (ignored) { }
     const newList = {
-      ["list-name"]: this.list_name,
-      superhero_ids: this.superhero_ids,
-      description: this.description,
-      visibility: this.visibility
-    }
+      user_id: this.currentUser?.user_id!,
+      ["list-name"]: this.listForm.value.list_name,
+      superhero_ids: JSON.parse(`[${this.listForm.value.superhero_ids}]`),
+      description: this.listForm.value.description,
+      visibility: this.listForm.value.visibility
+    };
     this.onAddList.emit(newList);
 
-    this.list_name = "";
-    this.superhero_ids = [];
-    this.description = "";
-    this.visibility = false;
+    // Reset the form
+    this.listForm.reset({
+      list_name: '',
+      superhero_ids: '',
+      description: '',
+      visibility: false
+    });
   }
-
 }
