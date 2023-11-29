1b2c1c68d78c1aad4b121e7b553d83acce7608b5
Christopher Higgins <chiggi24@uwo.ca>
2023-11-28 23:01:21 -0500

    Updated DCMA component
    Updated DCMA component with CSS and adjustments to new html assets.

--- client/app/src/app/components/dmca-compliance/dmca-compliance.component.ts
+++ client/app/src/app/components/dmca-compliance/dmca-compliance.component.ts
@@ -1,12 +1,22 @@
 // dmca-compliance.component.ts
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TitleService } from '../../services/title.service';
+import { UserService } from '../../services/user.service';
+import { User } from '../../user';
 
 @Component({
   selector: 'app-dmca-compliance',
   templateUrl: './dmca-compliance.component.html',
   styleUrls: ['./dmca-compliance.component.css']
 })
-export class DmcaComplianceComponent {
+export class DmcaComplianceComponent implements OnInit {
+  currentUser: User | null = null;
+  constructor(private titleService: TitleService, private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.titleService.setTitle("Copyright Enforcement")
+    this.userService.getCurrentUser().subscribe(user => this.currentUser = user)
+  }
   // Implement methods for submitting DMCA entries and managing reviews
   submitDmcaEntry() {
     // Implement logic to submit DMCA entry to the database
