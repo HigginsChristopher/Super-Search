2e4627904af673c946e22d19352529773fa44dc9
Christopher Higgins <chiggi24@uwo.ca>
2023-11-28 23:03:51 -0500

    Updated footer component
    Updated footer component to have nicer CSS, with policies and copyright symbol bottom centered, admin button bottom right and home button bottom left.
    
    Added functionality to route to admin menu and route home.

--- client/app/src/app/components/footer/footer.component.ts
+++ client/app/src/app/components/footer/footer.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../../services/user.service';
+import { User } from '../../user';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  currentUser: User | null = null;
+  constructor(private router: Router, private userService: UserService) { }
+  ngOnInit(): void {
+    this.userService.getCurrentUser().subscribe(user => this.currentUser = user);
+    
+  }
+  routeHome() {
+    this.router.navigate(['/']);
+  }
+  hasRoute(route: string) {
+    return this.router.url === route;
+  }
 
+  routeAdminMenu() {
+    const currentUrl = this.router.url;
+    const listsUrl = currentUrl.endsWith('/admin-menu') ? currentUrl : `${currentUrl}/admin-menu`;
+    this.router.navigate([listsUrl]);
+  }
 }
