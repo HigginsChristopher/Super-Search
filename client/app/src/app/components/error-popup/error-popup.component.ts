0a25d5ca8d04988d95024af9df68621921b25d94
Christopher Higgins <113563713+chiggi24@users.noreply.github.com>
2023-12-01 16:21:12 -0500

    Updated error popup component
    Added email verification resend and account recovery to errors generated during log in process.

--- client/app/src/app/components/error-popup/error-popup.component.ts
+++ client/app/src/app/components/error-popup/error-popup.component.ts
@@ -3,6 +3,7 @@ import { faTimes } from "@fortawesome/free-solid-svg-icons"
 import { UserService } from '../../services/user.service';
 import { User } from '../../user';
 import { Router } from '@angular/router';
+import { jwtDecode } from 'jwt-decode';
 
 @Component({
   selector: 'app-error-popup',
@@ -12,11 +13,14 @@ import { Router } from '@angular/router';
 export class ErrorPopupComponent implements OnInit {
   faTimes = faTimes;
   @Output() closePopup: EventEmitter<void> = new EventEmitter<void>();
+  @Output() deleteList: EventEmitter<void> = new EventEmitter<void>();
   @Input() errorMessage: any = '';
   @Input() currentUser: User | null = null;
   mockEmail = "";
   verifyUrl = "";
   registrationSuccess = false;
+  forgotSuccess = false;
+
 
   constructor(
     private userService: UserService,
@@ -39,13 +43,6 @@ export class ErrorPopupComponent implements OnInit {
     const host = window.location.host;
     const baseUrl = `${protocol}//${host}`;
     const url = `${baseUrl}/api/open/users/verify?token=${this.currentUser?.verificationToken}`
-    this.mockEmail = `
-      Mock Email:
-      From: Do Not Reply <myEmail@myService.myDomain>
-      To: <${this.currentUser?.email}>
-      Subject: Resent account confirmation. Please confirm account for SE3316 Lab 4
-      Text: Please confirm your account by clicking the following link: 
-    `
     this.verifyUrl = url;
   }
 
@@ -60,29 +57,36 @@ export class ErrorPopupComponent implements OnInit {
   onVerify() {
     this.userService.verifyUser(this.currentUser!.verificationToken!).subscribe(response => {
       localStorage.setItem('token', JSON.stringify(response.token));
-      const user = this.decodeJwt(response.token);
+      const user = jwtDecode(response.token)
       localStorage.setItem('userData', JSON.stringify(user));
-      this.userService.setUser(user);
+      this.userService.setUser(user as User);
       this.router.navigate(['/'])
     });
   }
 
 
-
-  decodeJwt(token: string) {
-    const base64Url = token.split('.')[1];
-    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    const jsonPayload = decodeURIComponent(
-      atob(base64)
-        .split('')
-        .map(function (c) {
-          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-        })
-        .join('')
-    );
-    return JSON.parse(jsonPayload);
+  onForgot() {
+    this.forgotSuccess = true;
+    const protocol = window.location.protocol;
+    const host = window.location.host;
+    const baseUrl = `${protocol}//${host}`;
+    const url = `${baseUrl}/api/open/users/recovery`
+    this.verifyUrl = url;
+  }
+  
+  onReset() {
+    this.userService.forgotPassword(this.currentUser!).subscribe(response => {
+      localStorage.setItem('token', JSON.stringify(response.token));
+      const user = jwtDecode(response.token)
+      localStorage.setItem('userData', JSON.stringify(user));
+      this.userService.setUser(user as User);
+      this.router.navigate(['/reset-password'])
+    }
+    )
   }
 
-  onForgot() { }
-
+  onDeleteList(){
+    document.body.classList.remove('modal-open');
+    this.deleteList.emit();
+  }
 }
